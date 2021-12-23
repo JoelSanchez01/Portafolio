1e1e7562132b2a24625889a95eaf90a2d9cdd1fa
JoelSanchez01 <joeleduardosanchezherrera@gmail.com>
2021-12-23 16:58:23 -0600

    se agrego la card vacia

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./assets/styles/App.css";
 import Header from "./components/Header";
-
+import Card from "./components/Card";
 class App extends React.Component {
   render() {
     return (
       <div id="app">
         <Header />
+        <div className="tarjeta">
+          <Card>
+            <h1>hola</h1>
+          </Card>
+        </div>
       </div>
     );
   }
